c862ce4dda78152532cc863e5e61b47001e5a89e
rmreyes-collab2 <reamayreyes07@gmail.com>
2026-05-26 13:57:33 +0800

    Create lab9.php

---
+++ lab9.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post" action="">
+    Name: <input type="text" name="name"><br><br>
+
+    Email: <input type="text" name="email"><br><br>
+
+    Gender:<br>
+    <input type="radio" name="gender" value="Male"> Male<br>
+    <input type="radio" name="gender" value="Female"> Female<br><br>
+
+    Course:
+    <select name="course">
+        <option value="">--Select Course--</option>
+        <option value="BSIT">BSIT</option>
+        <option value="BSCS">BSCS</option>
+        <option value="BSEd">BSEd</option>
+        <option value="BSBA">BSBA</option>
+    </select><br><br>
+
+    Message:<br>
+    <textarea name="message" rows="5" cols="30"></textarea><br><br>
+
+    <input type="submit">
+</form>
+
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    echo "Name: " . $name . "<br>";
+    echo "Email: " . $email . "<br>";
+
+    if(isset($_POST["gender"])) {
+        echo "Gender: " . $_POST["gender"] . "<br>";
+    } else {
+        echo "Gender is not selected<br>";
+    }
+
+    if(!empty($_POST["course"])) {
+        echo "Course: " . $_POST["course"] . "<br>";
+    } else {
+        echo "Course is not selected<br>";
+    }
+
+    if(!empty($message)) {
+        echo "Message: " . nl2br($message);
+    } else {
+        echo "Message is empty";
+    }
+}
+?>
+
+</body>
+</html>
