1e6395f9c77cfd44f459f8e7ffc1a1ab180dee46
rmreyes-collab2 <reamayreyes07@gmail.com>
2026-05-26 14:02:34 +0800

    Create lab19.php

---
+++ lab19.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Registration Form</h2>
+
+<form method="post" action="Lab 19 process.php">
+
+    Name: <input type="text" name="name" required><br><br>
+
+    Email: <input type="text" name="email" required><br><br>
+
+    Age: <input type="number" name="age" required><br><br>
+
+    Gender:<br>
+    <input type="radio" name="gender" value="Male" required> Male<br>
+    <input type="radio" name="gender" value="Female"> Female<br><br>
+
+    <input type="submit" value="Submit">
+
+</form>
+
+</body>
+</html>
